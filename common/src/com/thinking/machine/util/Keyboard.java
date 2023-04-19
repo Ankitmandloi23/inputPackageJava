3ad9fefbf3db1d22a9a446dc7dde6cd6518b6e8e
Ankit Mandloi <ankitmandloi23@gmail.com>
2023-04-19 13:52:13 +0530

    first commit

---
+++ common/src/com/thinking/machine/util/Keyboard.java
@@ -0,0 +1,147 @@
+package com.thinking.machine.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Keyboard {
+
+
+    private BufferedReader bufferedReader;
+
+    public Keyboard()
+    {
+        bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+    }
+
+
+    public String getString()  {
+        String str;
+        try{
+            str = bufferedReader.readLine();
+        }catch (IOException ioException)
+        {
+            str ="";
+        }
+        return str;
+        }
+    public String getString(String message) {
+        System.out.println(message);
+          return getString();
+    }
+
+
+    public char getCharacter()
+    {
+        return getString().charAt(0);
+    }
+    public char getCharacter(String message)
+    {
+        System.out.println(message);
+      return getCharacter();
+    }
+
+
+    public long getLong()
+    {
+        long x;
+        try
+        {
+            x = Long.parseLong(getString());
+        } catch (NumberFormatException numberFormatException)
+        {
+            x=0;
+        }
+        return x;
+    }
+
+
+    public long getLong(String message)
+    {
+        System.out.println(message);
+        return getLong();
+    }
+
+    public int getInt()
+    {
+        int x;
+        try
+        {
+            x = Integer.parseInt(getString());
+        } catch (NumberFormatException numberFormatException)
+        {
+            x=0;
+        }
+        return x;
+    }
+
+
+    public int getInt(String message)
+    {
+        System.out.println(message);
+        return getInt();
+    }
+
+
+    public short getShort()
+    {
+        short x;
+        try
+        {
+            x = Short.parseShort(getString());
+        } catch (NumberFormatException numberFormatException)
+        {
+            x=0;
+        }
+        return x;
+    }
+
+
+    public short getShort(String message)
+    {
+        System.out.println(message);
+        return getShort();
+    }
+
+    public float getFloat()
+    {
+        float x;
+        try
+        {
+            x = Float.parseFloat(getString());
+        } catch (NumberFormatException numberFormatException)
+        {
+            x=0;
+        }
+        return x;
+    }
+
+
+    public float getFloat(String message)
+    {
+        System.out.println(message);
+        return getFloat();
+    }
+
+
+    public boolean getBlooean()
+    {
+        boolean x;
+        try
+        {
+            x = Boolean.parseBoolean(getString());
+        } catch (NumberFormatException numberFormatException)
+        {
+            x=false;
+        }
+        return x;
+    }
+
+
+    public boolean getBlooean(String message)
+    {
+        System.out.println(message);
+        return getBlooean();
+    }
+}
+
---
+++ common/testcases/KeyBoardTestcase.java
@@ -0,0 +1,52 @@
+import com.thinking.machine.util.*;
+
+
+class KeyBoardTestcase {
+    public static void main(String gg[]) {
+        Keyboard k = new Keyboard();
+        String a;
+        System.out.println("enter a sting");
+        a = k.getString();
+
+        String b;
+        b = k.getString("enter another String:");
+
+        char c;
+        c = k.getCharacter("enter gender(M/F):");
+
+        long d;
+        d = k.getLong("enter long type value:");
+
+        int e;
+        e = k.getInt("enter a int type value:");
+
+        //byte f;
+        //f = k.getByte("enter a byte type value:");
+
+        short g;
+        g = k.getShort("enter a short type value:");
+
+//        double h;
+//        h = k.getDouble("enter double type value:");
+
+
+       // boolean i;
+        //i = k.getBoolean("enter a boolean type value:");
+
+        float j;
+        j = k.getFloat("enter float type value:");
+
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        //System.out.println(f);
+        System.out.println(g);
+        //System.out.println(h);
+        //System.out.println(i);
+        System.out.println(j);
+
+    }
+}
